7ef027e2220db9ebd19181982af0ca254b99dae1
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-15 19:18:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board,index)
-  spot = postion.to_i
+  spot = index.to_i
   if spot.between?(0,8) == true && position_taken?(board, index) == false
     return true
   else
