d114272347eacff53fcc07dce913dd2517d44042
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-15 18:22:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board,index)
-  if board[index].between(1,9) == true && position_taken? == false
+  if board[index].between(1,9) == true && position_taken? return false
     return true
   else
     return false
